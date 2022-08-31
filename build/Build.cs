f5c403cee0f5e410632323033e90ce9f676289de
anktsrkr <ankt.srkr@gmail.com>
2022-08-31 13:37:24 +0100

    Add project files.

---
+++ build/Build.cs
@@ -0,0 +1,196 @@
+using System.Linq;
+
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Tools.NerdbankGitVersioning;
+
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+
+[GitHubActions("continuous",
+    GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = true,
+    FetchDepth = 0,
+    OnPushBranches = new[] 
+    {
+        "main", 
+        "dev",
+        "releases/**"
+    },
+    OnPullRequestBranches = new[] 
+    {
+        "releases/**" 
+    },
+    InvokedTargets = new[]
+    {
+        nameof(Pack),
+    },
+    EnableGitHubToken = true,
+    ImportSecrets = new[] 
+    { 
+        nameof(MyGetApiKey), 
+        nameof(NuGetApiKey) 
+    }
+)]
+
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Pack);
+    
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("MyGet Feed Url for Public Access of Pre Releases")]
+    readonly string MyGetNugetFeed;
+    [Parameter("MyGet Api Key"), Secret]
+    readonly string MyGetApiKey;
+
+    [Parameter("Nuget Feed Url for Public Access of Pre Releases")]
+    readonly string NugetFeed;
+    [Parameter("Nuget Api Key"), Secret]
+    readonly string NuGetApiKey;
+
+    [Parameter("Copyright Details")]
+    readonly string Copyright;
+
+    [Parameter("Artifacts Type")]
+    readonly string ArtifactsType;
+
+    [Parameter("Excluded Artifacts Type")]
+    readonly string ExcludedArtifactsType;
+
+    [GitVersion]
+    readonly GitVersion GitVersion;
+
+    [GitRepository]
+    readonly GitRepository GitRepository;
+
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+
+    static GitHubActions GitHubActions => GitHubActions.Instance;
+    static AbsolutePath ArtifactsDirectory => RootDirectory / ".artifacts";
+
+    string GithubNugetFeed => GitHubActions != null
+         ? $"https://nuget.pkg.github.com/{GitHubActions.RepositoryOwner}/index.json"
+         : null;
+
+
+    Target Clean => _ => _
+      .Description($"Cleaning Project.")
+      .Before(Restore)
+      .Executes(() =>
+      {
+          DotNetClean(c => c.SetProject(Solution.src.Sundry_HelloWorld));
+          EnsureCleanDirectory(ArtifactsDirectory);
+      });
+    Target Restore => _ => _
+        .Description($"Restoring Project Dependencies.")
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(
+                r => r.SetProjectFile(Solution.src.Sundry_HelloWorld));
+        });
+
+    Target Compile => _ => _
+        .Description($"Building Project with the version.")
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(b => b
+                .SetProjectFile(Solution.src.Sundry_HelloWorld)
+                .SetConfiguration(Configuration)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .EnableNoRestore());
+        });
+
+    Target Pack => _ => _
+    .Description($"Packing Project with the version.")
+    .Requires(() => Configuration.Equals(Configuration.Release))
+    .Produces(ArtifactsDirectory / ArtifactsType)
+    .DependsOn(Compile)
+    .Triggers(PublishToGithub, PublishToMyGet, PublishToNuGet)
+    .Executes(() =>
+    {
+        DotNetPack(p =>
+            p
+                .SetProject(Solution.src.Sundry_HelloWorld)
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .SetCopyright(Copyright)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetFileVersion(GitVersion.AssemblySemFileVer));
+    });
+
+    Target PublishToGithub => _ => _
+       .Description($"Publishing to Github for Development only.")
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnDevelopBranch() || GitHubActions.IsPullRequest)
+       .Executes(() =>
+       {
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+               .Where(x => !x.EndsWith(ExcludedArtifactsType))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(GithubNugetFeed)
+                       .SetApiKey(GitHubActions.Token)
+                       .EnableSkipDuplicate()
+                   );
+               });
+       });
+
+    Target PublishToMyGet => _ => _
+       .Description($"Publishing to MyGet for PreRelese only.")
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnReleaseBranch())
+       .Executes(() =>
+       {
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+               .Where(x => !x.EndsWith(ExcludedArtifactsType))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(MyGetNugetFeed)
+                       .SetApiKey(MyGetApiKey)
+                       .EnableSkipDuplicate()
+                   );
+               });
+       });
+    Target PublishToNuGet => _ => _
+       .Description($"Publishing to NuGet with the version.")
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnMainOrMasterBranch())
+       .Executes(() =>
+       {
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+               .Where(x => !x.EndsWith(ExcludedArtifactsType))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetFeed)
+                       .SetApiKey(NuGetApiKey)
+                       .EnableSkipDuplicate()
+                   );
+               });
+       });
+}
---
+++ src/Say.cs
@@ -0,0 +1,7 @@
+﻿namespace Sundry.HelloWorld
+{
+    public static class Say
+    {
+        public static void Hello() => Console.WriteLine("Hello World!");
+    }
+}
